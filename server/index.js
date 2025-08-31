3809191fa78cd8dd9017f72c42b0cb236ec2b4c7
Ghislain Byimbo <byimbog250@gmail.com>
2025-08-31 18:04:51 +0200

    18 commit

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ const ws = require('ws');
 const server = new ws.Server({ port: '3000' });
 
 server.on("connection", (socket) => {
+    console.log("client connect on the server");
     socket.on("message", (message) => {
-        console.log(message);
+        const b = Buffer.from(message)
+        console.log(b.toString());
         socket.send(`${message}`);
     })
 })
