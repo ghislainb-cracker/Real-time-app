626b4fbb9f22835e53b2874d182935e633329c17
Ghislain Byimbo <byimbog250@gmail.com>
2025-08-31 17:47:38 +0200

    8 commit

---
+++ app/script.js
@@ -0,0 +1,12 @@
+const socket = new WebSocket('ws://localhost:3000');
+
+function sendMessage(e){
+    e.preventDefault();
+    const input = document.querySelector('input');
+    
+    
+}
+
+
+   
+
