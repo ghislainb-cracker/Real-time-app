9cbe0efe048fc5063dfd599d4c4295e2a22af73b
Ghislain Byimbo <byimbog250@gmail.com>
2025-09-04 22:20:13 +0200

    27 commit

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const server = new ws.Server({ port: '3000' })
 server.on("connection", (socket) => {
     console.log("Client connected on the server successfully✅");
     socket.on("message", (msg) => {
-       
+        console.log("received message is:", msg.toString())
         
     })
 })
