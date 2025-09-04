555bce7a69f43a5d537fb998fc07067598bf4d9a
Ghislain Byimbo <byimbog250@gmail.com>
2025-09-04 22:17:47 +0200

    19 commit

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
-const ws = require('ws');
-const server = new ws.Server({ port: '3000' });
+// const ws = require('ws');
+// const server = new ws.Server({ port: '3000' });
+
+// server.on("connection", (socket) => {
+//     console.log("client connect on the server");
+//     socket.on("message", (message) => {
+//         console.log("received message", message.toString());
+//         socket.send(message.toString());
+//     })
+// })
+
+const ws = require('ws')
+const server = new ws.Server({ port: '3000' })
 
 server.on("connection", (socket) => {
-    console.log("client connect on the server");
-    socket.on("message", (message) => {
-        const b = Buffer.from(message)
-        console.log(b.toString());
-        socket.send(`${message}`);
+    console.log("Client connected on the server successfully✅");
+    socket.on("message", (msg) => {
+        console.log("received message is:", msg.toString())
+        socket.send(msg.toString());
     })
 })
